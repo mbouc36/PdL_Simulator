3bc9b5676260806f88f81cb4f70b62278441bfb4
mbouc36 <mbouc36@gmail.com>
2026-02-24 15:37:49 -0500

    Updated code to work with serial plotter

--- test_sensors/IMU_test/IMU_test.ino
+++ test_sensors/IMU_test/IMU_test.ino
@@ -27,22 +27,35 @@ void loop() {
   imuMag.read();
 
   // Print Accel
-  Serial.print("A: ");
-  Serial.print(imu6.a.x); Serial.print(" ");
-  Serial.print(imu6.a.y); Serial.print(" ");
-  Serial.print(imu6.a.z); Serial.print(" ");
+
+  Serial.print("A_X:");
+  Serial.print(imu6.a.x);
+  Serial.print(" ");
+  Serial.print("A_Y:");
+  Serial.print(imu6.a.y);
+  Serial.print(" ");
+  Serial.print("A_Z:");
+  Serial.println(imu6.a.z);
 
   // Print Gyro
-  Serial.print("G: ");
-  Serial.print(imu6.g.x); Serial.print(" ");
-  Serial.print(imu6.g.y); Serial.print(" ");
-  Serial.print(imu6.g.z); Serial.print(" ");
+  Serial.print("G_X:");
+  Serial.print(imu6.g.x);
+  Serial.print(" ");
+  Serial.print("G_Y:");
+  Serial.print(imu6.g.y);
+  Serial.print(" ");
+  Serial.print("G_Z:");
+  Serial.println(imu6.g.z);
 
-  // Print Mag
-  Serial.print("M: ");
-  Serial.print(imuMag.m.x); Serial.print(" ");
-  Serial.print(imuMag.m.y); Serial.print(" ");
-  Serial.print(imuMag.m.z);
+  // // Print Mag allo
+  Serial.print("M_X:");
+  Serial.print(imuMag.m.x);
+  Serial.print(" ");
+  Serial.print("M_Y:");
+  Serial.print(imuMag.m.y);
+  Serial.print(" ");
+  Serial.print("M_Z:");
+  Serial.println(imuMag.m.z);
   
   Serial.println();
   delay(100);
