924b5225754b55c2abb0eb766d29ba18b397215a
mbouc36 <mbouc36@gmail.com>
2026-06-09 11:47:20 -0400

    Refactored IMU class to be more portable

--- data_processing/body_rotation.py
+++ data_processing/body_rotation.py
@@ -1,13 +1,19 @@
 import serial
 import time
+import json
+import math
 import socket
 import numpy as np
 from ahrs.filters import Madgwick
 from ahrs.common.quaternion import Quaternion
-from imu_helper_functions import get_imu_data, get_calibration_data
 import os
 import sys
 
+GAUSS_TO_MILLI_TESLA_CONVERSION = 10
+CONFIG_FILENAME = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "../calibration/imu_calibration/cal_data.json",
+)
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 from update_config import load_config
@@ -34,6 +40,57 @@ class IsolatedBodyAxisTracker:
         self.body_x_deg = 0.0
         self.body_y_deg = 0.0
         self.body_z_deg = 0.0
+
+        self.accOffset = None
+        self.accScale = None
+        self.gOffset = None
+        self.magOffset = None
+        self.magScale = None
+
+    def load_calibration_data(self, name="left", filename=CONFIG_FILENAME):
+        """
+        Read data from filename for IMU offset and scale data
+        """
+        with open(filename, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        data = data[name]
+
+        self.accOffset = data["self.accOffset"]
+        self.accScale = data["self.accScale"]
+        self.gOffset = data["self.gOffset"]
+        self.magOffset = data["self.magOffset"]
+        self.magScale = data["self.magScale"]
+
+    def get_imu_data(self, values):
+        """
+        Read Raw Sensor Data and use calibrated values
+        """
+        ax, ay, az, gx, gy, gz, mx, my, mz = map(float, values)
+
+        axCal = (ax - self.accOffset["x"]) * self.accScale["x"]
+        ayCal = (ay - self.accOffset["y"]) * self.accScale["y"]
+        azCal = (az - self.accOffset["z"]) * self.accScale["z"]
+
+        gxCal = math.radians(gx - self.gOffset["x"])
+        gyCal = math.radians(gy - self.gOffset["y"])
+        gzCal = math.radians(gz - self.gOffset["z"])
+
+        mxCal = (
+            (mx - self.magOffset["x"]) * self.magScale["x"]
+        ) / GAUSS_TO_MILLI_TESLA_CONVERSION
+        myCal = (
+            (my - self.magOffset["y"]) * self.magScale["y"]
+        ) / GAUSS_TO_MILLI_TESLA_CONVERSION
+        mzCal = (
+            (mz - self.magOffset["z"]) * self.magScale["z"]
+        ) / GAUSS_TO_MILLI_TESLA_CONVERSION
+
+        gyro_data = np.array([gxCal, gyCal, gzCal])
+        acc_data = np.array([axCal, ayCal, azCal])
+        mag_data = np.array([mxCal, myCal, mzCal])
+
+        return gyro_data, acc_data, mag_data
 
     def update(self, gyro, accel, mag):
         # 1. Get the global, drift-corrected quaternion from Madgwick
@@ -63,6 +120,26 @@ class IsolatedBodyAxisTracker:
 
         return self.body_x_deg, self.body_y_deg, self.body_z_deg
 
+    def get_angles(self, line):
+        values = line.split(",")
+
+        if len(values) != 9:
+            return
+
+        # 1. Grab the fresh data from the IMU
+        gyro, accel, mag = self.get_imu_data(values)
+
+        # 2. Feed the vectors into the filter and get the delta-calculated Z angle
+        angles = self.update(gyro, accel, mag)
+
+        # 3. Print or use your perfectly drift-corrected body-frame rotation
+        # self.body_x_deg, self.body_y_deg, self.body_z_deg
+        msg = f"{angles[0].item():.4f}, {angles[1].item():.4f}, {angles[2].item():.4f}"
+        sock.sendto(msg.encode("utf-8"), (UDP_IP, UDP_PORT))
+        print(msg)
+
+        return angles
+
 
 def get_x_y_z_angles():
     try:
@@ -75,33 +152,17 @@ def get_x_y_z_angles():
     tracker = IsolatedBodyAxisTracker()
 
     # Fetch calibration data from json
-    cal_data = get_calibration_data()
+    tracker.load_calibration_data()
 
     try:
         while True:
-            t = time.time()
             try:
                 line = ser.readline().decode("utf-8").strip()
             except Exception as e:
                 print(e)
                 continue
 
-            values = line.split(",")
-
-            if len(values) != 9:
-                continue
-
-            # 1. Grab the fresh data from the IMU
-            gyro, accel, mag = get_imu_data(values, cal_data)
-
-            # 2. Feed the vectors into the filter and get the delta-calculated Z angle
-            angles = tracker.update(gyro, accel, mag)
-
-            # 3. Print or use your perfectly drift-corrected body-frame rotation
-            # self.body_x_deg, self.body_y_deg, self.body_z_deg
-            msg = f"{angles[0].item():.4f}, {angles[1].item():.4f}, {angles[2].item():.4f}"
-            sock.sendto(msg.encode("utf-8"), (UDP_IP, UDP_PORT))
-            print(msg)
+            tracker.get_angles(line)
 
     except KeyboardInterrupt:
         print("\nTracking stopped.")
